cd704fe802bf631a9456c3e6d4ec23b07a797d7e
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-10-16 22:42:26 -0700

    form broke again:

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,32 +1,56 @@
 <template>
   <form
-    method="POST" name="contact" data-netlify="true" v-on:submit.prevent="handleSubmit"
-  action="/success/"
-  data-netlify="true"
-  data-netlify-honeypot="bot-field">
-    <input type="hidden" name="form-name" :value="name" />
-      <p>The contents of this form will be sent directly to my inbox, and I will be in touch with you shortly.</p>
+    method="POST"
+    name="contact"
+    data-netlify="true"
+    action="/success/"
+    @submit.prevent="handleSubmit"
+  >
+    <input
+      type="hidden"
+      name="form-name"
+      :value="name"
+    >
+    <p>The contents of this form will be sent directly to my inbox, and I will be in touch with you shortly.</p>
 
-      <p hidden>
-        <label>
-          Don’t fill this out: <input name="bot-field" />
-        </label>
-      </p>
+    <div>
+      <label for="name">Name <input
+        v-model="formData.name"
+        type="text"
+        name="name"
+      >
+    </label>
+    </div>
+    <div>
+      <label for="email">
+        Email
+        <input
+        v-model="formData.email"
+        type="email"
+        name="email"
+      >
+    </label>
+    </div>
 
-
-  <div>
-    <label>Name <input type="text" name="name" v-model="formData.name" /></label>
-  </div>
-  <div>
-    <label>Email <input type="email" name="email" v-model="formData.email" /></label>
-  </div>
-
-  <label for="message">Message<textarea  name="message" v-model="formData.message" /></label>
-  <div>
-      <button @click="close" class="button--secondary">Cancel</button>
-    <button type="submit" @click="submit" class="button--primary">Send</button>
-  </div>
-</form>
+    <label for="message">Message<textarea
+      v-model="formData.message"
+      name="message"
+    /></label>
+    <div>
+      <button
+        class="button--secondary"
+        @click="close"
+      >
+        Cancel
+      </button>
+      <button
+        type="submit"
+        class="button--primary"
+      >
+        Send
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -38,26 +62,26 @@ export default {
   },
   methods: {
     encode(data) {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&')
-  },
-  handleSubmit(e) {
-    this.$emit('submit');
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: this.encode({
-        'form-name': e.target.getAttribute('name'),
-        ...this.formData,
-      }),
-    })
-    .then(() => this.$router.push('/success'))
-    .catch(error => alert(error))
-  },
-    close() {
-      this.$emit('close');
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
     },
+    handleSubmit(e) {
+      this.$emit('submit');
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData,
+        }),
+      })
+      .then(() => this.$router.push('/success'))
+      .catch(error => alert(error))
+    },
+      close() {
+        this.$emit('close');
+      },
+    }
   }
-}
 </script>
