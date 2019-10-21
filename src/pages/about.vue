849190f64d319f7a2f4f51d9405ebd6dc3f829ea
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-10-20 19:32:49 -0700

    add resume

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,81 @@
 <template>
-  <h1>About</h1>
+  <div>
+    <h1>About</h1>
+    <div class="about-container">
+      <p>
+        I'm Macguire — an experience designer, web developer, hackathon
+        participant, and music producer currently designing for Visier's
+        analytics platform.
+      </p>
+      <div class="about__photo">
+        <img
+          src="https://pbs.twimg.com/profile_images/1169836865379172352/RPj3FlZE.jpg"
+          alt="A photo of me."
+        />
+      </div>
+    </div>
+    <div class="content resume">
+      <h2>Resume</h2>
+      <h3>Experience</h3>
+      <div class="resume__context">
+        <h4>User Experience Design Intern</h4>
+        <span class="resume__date">May 2019 – December 2019</span>
+        <span class="resume__company">Visier</span>
+      </div>
+      <div class="resume__responsibilities">
+        <ul>
+          <li>
+            creating and improving data visualizations allowing Visier's 50,000
+            users to analyze over 6 million employee records
+          </li>
+          <li>
+            improving usability by designing interfaces and interactions that
+            follow conventions and best practices
+          </li>
+          <li>
+            collaborating with product and solution managers to understand use
+            cases and user needs
+          </li>
+          <li>
+            communicating design decisions and problem-solving with developers
+            to ship features and improvements
+          </li>
+        </ul>
+      </div>
+      <div class="resume__context">
+        <h4>Systems Analyst</h4>
+        <span class="resume__date">January 2019 – present</span>
+        <span class="resume__company">Simon Fraser University</span>
+      </div>
+      <div class="resume__responsibilities">
+        <ul>
+          <li>
+            maintain the lab's RHEL server and software (Apache, phpMyAdmin,
+            MySQL)
+          </li>
+          <li>maintain various WordPress and PMWiki instances</li>
+          <li>modify plugins and themes to fit various requirements</li>
+        </ul>
+      </div>
+      <div class="resume__context">
+        <h4>Systems Analyst</h4>
+        <span class="resume__date">September 2017 – March 2019</span>
+        <span class="resume__company">Vancouver Coastal Health</span>
+      </div>
+      <div class="resume__responsibilities">
+        <ul>
+          <li>
+            designing & developing web apps to support process improvement
+          </li>
+          <li>providing technical recommendations for digital projects</li>
+          <li>
+            designing & developing several websites to support staff education
+          </li>
+          <li>
+            prototyping VR projects for Cardboard and Oculus Go
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
