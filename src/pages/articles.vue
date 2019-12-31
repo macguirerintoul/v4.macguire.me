358741e00c0436f5822c5c03a585559743952bde
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-12-31 14:58:03 -0800

    nicer looking links

--- src/pages/articles.vue
+++ src/pages/articles.vue
@@ -2,14 +2,14 @@
   <section>
     <h1>Articles</h1>
     <!-- List posts -->
-    <table>
-      <tr v-for="edge in $page.articles.edges" :key="edge.node.id">
-        <td>
+    <table class="articles">
+      <tr class="articles__row" v-for="edge in $page.articles.edges" :key="edge.node.id">
+        <td class="articles__title">
           <g-link :to="edge.node.path">
             {{ edge.node.title }}
           </g-link>
         </td>
-        <td>{{ updated(edge.node.date) }}</td>
+        <td class="articles__updated">{{ updated(edge.node.date) }}</td>
       </tr>
     </table>
   </section>
