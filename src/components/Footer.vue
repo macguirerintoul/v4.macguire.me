826e33643151888bfd22ef51c1a8bc664c9f122f
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-02-15 09:58:18 -0800

    footer cohesive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,50 +1,6 @@
 <template>
   <footer>
     <div class="footer-content">
-      <section class="footer__left">
-        <div>
-        <h2>Macguire Rintoul</h2>
-        <p>Experience designer & developer</p>
-
-      </div>
-      <div class="footer__links">
-        <MagicLink url="/contact">Contact</MagicLink>
-        <MagicLink url="https://github.com/macguirerintoul/">GitHub</MagicLink>
-        <MagicLink url="https://www.linkedin.com/in/macguirerintoul/"
-          >LinkedIn</MagicLink
-        >
-        <MagicLink url="https://www.twitter.com/macguirerintoul/"
-          >Twitter</MagicLink
-        >
-        <MagicLink url="https://www.instagram.com/macguirerintoul/"
-          >Instagram</MagicLink>
-    </div>
-      <!-- <div>
-        <h3>Care for a quarterly email?</h3>
-        <div
-          v-if="subscriptionStatus != ''"
-          :class="['info-block', infoBlockStatus]"
-          v-text="subscriptionMessage"
-        />
-        <label class="footer-subscribe-label" for="subscribe-input" hidden>
-          Email
-        </label>
-        <form class="footer-form" @submit.prevent="addSubsriber">
-          <input
-            id="subscribe-input"
-            v-model="email"
-            class="footer-subscribe-input"
-            type="email"
-            placeholder="magic.johnson@gmail.com"
-            required
-          />
-          <button class="button--primary">
-            Subscribe
-          </button>
-        </form>
-      </div> -->
-      </section>
-
       <section class="featured-block-container">
         <FeaturedBlock type="randomStar" />
         <FeaturedBlock type="lastCommit" />
