49a81c5ad63599631ae49b53b0dfe9b6caff4c15
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-02-15 09:20:09 -0800

    replace type

--- src/components/ProjectBlock.vue
+++ src/components/ProjectBlock.vue
@@ -1,7 +1,5 @@
 <template>
   <g-link :class="['project-block', project.title]" :to="project.path">
-    <div :style="'height: 300px;background: url(https://res.cloudinary.com/macguire/image/upload/c_scale,h_600,dpr_auto,f_auto,q_auto:low/' +  project.imagePath + '.jpg) no-repeat center center/cover; }'">
-    </div>
     <div class="project-block__content">
       <h2 class="project-block--title">{{ project.title }}</h2>
       <div class="project-block__tags">
