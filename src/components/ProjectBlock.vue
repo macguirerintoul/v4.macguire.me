52e2cd9ab899ac6ecca320b88f6f0463bbb46560
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-05 19:14:23 -0800

    remove unused styled

--- src/components/ProjectBlock.vue
+++ src/components/ProjectBlock.vue
@@ -1,15 +1,14 @@
 <template>
   <g-link :class="['project-block', project.title]" :to="project.path">
-    <div class="project-block__content">
       <h2 class="project-block--title">{{ project.title }}</h2>
       <small class="project-block__tags">{{ project.tags.join(', ') }}</small>
       <p class="project-block__description">{{ project.shortDescription }}</p>
 
       <MagicImage
+				class="project-block__image"
         :path="project.imagePath"
         :alt="'Screenshot of ' + project.title"
       />
-    </div>
   </g-link>
 </template>
 
