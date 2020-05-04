d8ba3456f242be7e719560cf006d44608f6b8ce0
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-05-04 16:56:35 -0700

    about to remove css grid from project pages

--- src/components/ProjectOverview.vue
+++ src/components/ProjectOverview.vue
@@ -1,54 +1,54 @@
 <template>
-  <div>
-  <section class="project-overview">
-    <h1 class="project-overview__title">{{ project.title }}</h1>
-      <div class="project-overview__subtitle">
-				<span>{{project.tags.join(', ')}} — {{ project.year }}</span>
-      </div>
-    <p class="project-overview__summary" v-html="project.summary" />
+	<div>
+		<section class="project-overview">
+			<h1 class="project-overview__title">{{ project.title }}</h1>
+			<small class="project-overview__subtitle">
+				{{ project.year }} — {{ project.tags.join(", ") }}
+			</small>
+			<p class="project-overview__summary" v-html="project.summary" />
 
-    <div class="project-overview__image">
-      <MagicImage
-        :path="project.imagePath"
-        :alt="'Screenshot of ' + project.title"
-      />
-    </div>
-      <div>
-        <h4>For</h4>
-        <span>{{ project.for }}</span>
-      </div>
-      <div>
-        <h4>Roles</h4>
-        <span>{{ project.roles.join(', ') }}</span>
-      </div>
-      <div>
-        <h4>Tools</h4>
-        <span>{{ project.tools.join(', ') }}</span>
-      </div>
-      <div>
-        <h4>Interested?</h4>
-        <span v-html="project.interested"></span>
-      </div>
-  </section>
-    </div>
+			<div class="project-overview__image">
+				<MagicImage
+					:path="project.imagePath"
+					:alt="'Screenshot of ' + project.title"
+				/>
+			</div>
+			<div>
+				<h4>For</h4>
+				<span>{{ project.for }}</span>
+			</div>
+			<div>
+				<h4>Roles</h4>
+				<span>{{ project.roles.join(", ") }}</span>
+			</div>
+			<div>
+				<h4>Tools</h4>
+				<span>{{ project.tools.join(", ") }}</span>
+			</div>
+			<div>
+				<h4>Interested?</h4>
+				<span v-html="project.interested"></span>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-import MagicImage from './MagicImage'
-import MagicLink from './MagicLink'
+import MagicImage from "./MagicImage";
+import MagicLink from "./MagicLink";
 
 export default {
-  components: {
-    MagicImage,
-    MagicLink,
-  },
-  props: {
-    project: {
-      type: Object,
-      default() {
-        return {}
-      },
-    },
-  },
-}
+	components: {
+		MagicImage,
+		MagicLink
+	},
+	props: {
+		project: {
+			type: Object,
+			default() {
+				return {};
+			}
+		}
+	}
+};
 </script>
