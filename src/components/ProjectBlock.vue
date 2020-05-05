91010c8486bf2ccb4110c79624b057c2ba75ffdb
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-05-05 14:01:30 -0700

    refactoring variables

--- src/components/ProjectBlock.vue
+++ src/components/ProjectBlock.vue
@@ -1,6 +1,6 @@
 <template>
 	<g-link class="project-block" :to="project.path">
-		<h2 class="project-block--title">{{ project.title }}</h2>
+		<h2 class="project-block__title">{{ project.title }}</h2>
 		<p class="project-block__description">{{ project.shortDescription }}</p>
 		<div class="project-block__context">
 			<small>{{ project.year }} – {{ project.tags.join(", ") }}</small>
