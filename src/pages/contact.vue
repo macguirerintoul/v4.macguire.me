522d85b7bbe5927d54310dc70313d016bcb589e5
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-05-07 19:37:05 -0700

    recoded contact form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,66 +1,73 @@
 <template>
-	<section>
+	<div>
 		<h1>Contact</h1>
-		<div class="contact-form">
-			<div v-if="submitted === 'error'" class="info-block info-block--error">
-				<p class="info-block__title">
-					An error occurred.
-				</p>
-				<p class="info-block__description">
-					Are you connected to the Internet?
-				</p>
-			</div>
-			<form
-				v-if="submitted != true"
-				method="POST"
-				name="contact"
-				data-netlify="true"
-				@submit.prevent="handleSubmit"
-			>
-				<input type="hidden" name="form-name" value="contact" />
-				<p>
-					The contents of this form will be sent directly to my inbox, and I
-					will be in touch with you shortly.
-				</p>
-
-				<label for="name">Name</label>
-				<input v-model="formData.name" type="text" name="name" />
-				<label for="email">Email</label>
-				<input v-model="formData.email" type="email" name="email" required />
-
-				<label for="message">Message</label>
-				<textarea v-model="formData.message" name="message" required />
-
-				<div class="form__button-container">
-					<button type="button" class="button--secondary" @click="close">
-						Cancel
-					</button>
-					<button type="submit" class="button--primary">
-						Send
-					</button>
-				</div>
-			</form>
-
-			<div v-if="submitted === true">
-				<h2>Message sent.</h2>
-				<p>Thanks for reaching out. I will be in touch with you shortly.</p>
-				<g-link to="/blog">
-					How about a blog post?
-				</g-link>
-			</div>
+		<div
+			v-if="submissionState === 'error'"
+			class="info-block info-block--error"
+		>
+			<p class="info-block__title">
+				An error occurred 😳
+			</p>
 		</div>
-	</section>
+		<!-- netlify attribute connects the form to Netlify Forms -->
+		<form
+			v-if="submissionState === 'default'"
+			class="contact-form"
+			method="POST"
+			name="Contact"
+			action="/"
+			netlify
+			@submit.prevent="handleSubmit"
+		>
+			<p>
+				Here's a contact form that goes straight to my inbox. Or, feel free to
+				email me at firstname.lastname@gmail.com if you believe
+				<MagicLink
+					url="https://twitter.com/zhayitong/status/1257844300206927872"
+					>forms are for customer support that never get back to you</MagicLink
+				>.
+			</p>
+
+			<label for="name">Name</label>
+			<input v-model="formData.name" type="text" name="name" />
+
+			<label for="email">Email</label>
+			<input v-model="formData.email" type="email" name="email" required />
+
+			<label for="message">Message</label>
+			<textarea v-model="formData.message" name="message" required />
+
+			<div class="form__button-container">
+				<button type="submit" class="button--primary">
+					Send
+				</button>
+			</div>
+		</form>
+
+		<div v-if="submissionState === 'submitted'">
+			<h2>Message sent.</h2>
+			<p>Thanks for reaching out. I will be in touch with you shortly.</p>
+			<g-link to="/blog">
+				How about a blog post?
+			</g-link>
+		</div>
+	</div>
 </template>
 
 <script>
+import MagicLink from "~/components/MagicLink";
+
 export default {
+	components: {
+		MagicLink
+	},
 	metaInfo: {
 		title: "Contact"
 	},
 	data() {
 		return {
-			submitted: false,
-			formData: {}
+			submissionState: "default",
+			formData: { name: "", email: "", message: "", "form-name": "Contact" }
 		};
 	},
 	methods: {
@@ -71,22 +78,22 @@ export default {
 				)
 				.join("&");
 		},
-		handleSubmit(e) {
-			// Post the form data to '/' where Netlify forms will pick it up
+		handleSubmit(event) {
+			// Post the form data to '/' where Netlify Forms will pick it up
 			fetch("/", {
 				method: "POST",
 				headers: { "Content-Type": "application/x-www-form-urlencoded" },
 				body: this.encode({
-					"form-name": e.target.getAttribute("name"),
+					"form-name": event.target.getAttribute("name"),
 					...this.formData
 				})
 			})
 				.then(data => {
 					console.log(data);
 					if (data.status == 200) {
-						this.submitted = true;
-					} else {
-						this.submitted = "error";
+						this.submissionState = "submitted";
+					} else if (data.status == 401) {
+						this.submissionState = "401";
 					}
 				})
 				.catch(error => {
@@ -94,9 +101,6 @@ export default {
 					this.submitted = "error";
 					alert(error);
 				});
-		},
-		close() {
-			this.$parent.$emit("close");
 		}
 	}
 };
