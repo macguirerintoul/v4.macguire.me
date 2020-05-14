3a68a6b500d2af9701bc4f9280d74a516bed5c96
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-05-13 22:49:06 -0700

    refactor magic video

--- src/components/MagicVideo.vue
+++ src/components/MagicVideo.vue
@@ -1,26 +1,25 @@
 <template>
-	<div>
+	<div class="magic-video">
 		<div v-if="source == 'vimeo'" class="video-embed">
+			<!-- to be responsive, the iframe requires a wrapper -->
 			<iframe
 				:src="'https://player.vimeo.com/video/' + path + '?byline=0&portrait=0'"
 				frameborder="0"
-				allow="autoplay; fullscreen"
+				allow="fullscreen"
 				allowfullscreen
 			></iframe>
 		</div>
-		<div v-if="source == 'cloudinary'">
-			<CldVideo
-				class="magic-video"
-				cloud-name="macguire"
-				muted
-				loop="true"
-				autoplay="autoplay"
-				lazy
-				:public-id="path"
-			>
-				<CldTransformation quality="auto:eco" fetch-format="auto" />
-			</CldVideo>
-		</div>
+		<CldVideo
+			v-if="source == 'cloudinary'"
+			cloud-name="macguire"
+			muted
+			loop="true"
+			autoplay="autoplay"
+			lazy
+			:public-id="path"
+		>
+			<CldTransformation quality="auto:eco" fetch-format="auto" />
+		</CldVideo>
 	</div>
 </template>
 
