0cfd4c285f8b8efb0fba574c88b7e089082e9706
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-06-06 18:23:19 -0700

    start to refactor blog page

---
+++ src/components/TDTBlock.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>{{ title }}</h2>
+		<p>{{ description }}</p>
+		<small>{{ tags }}</small>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title,
+		description,
+		tags
+	}
+}
+</script>
